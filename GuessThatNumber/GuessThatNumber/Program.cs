783369e9cc5b903d3cfb85cf7340d67e30999588
SeedPaths-User <lessdismissive@email.com>
2014-09-21 22:01:02 -0600

    GuessThatNumber

--- GuessThatNumber/GuessThatNumber/Program.cs
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -14,17 +14,16 @@ namespace GuessThatNumber
             Console.WriteLine("Lets play GUESS THAT NUMBER!!!\n\nTry to find the correct number by typing in a number from 1 to 100.\n\n");
 
             //call our guessThatNumber function
-            guessThatNumber("\n");
+            guessThatNumber();
 
             //keep console open
             Console.ReadKey(true);
         }
 
         //functions go here
-        static void guessThatNumber(string input)
+        static void guessThatNumber()
         {
-            //find the users input with .readline
-            int Guess = int.Parse(Console.ReadLine());
+            
 
             //create randomnumbergenerator
             Random randNum = new Random();
@@ -35,33 +34,91 @@ namespace GuessThatNumber
             //claim a variable for the number of guesses
             int numGuesses = 0;
             
-            //claim a boolean as true to run our while loop
-            bool boolGuess = true;
+            //claim a boolean as (false) to run our while loop
+            bool hasWon = false;
             
             //create a loop while the user guess is true
-            while (boolGuess)
+            while (!hasWon)
             {
+                //find the users input with .readline
+                int Guess = int.Parse(Console.ReadLine());
+
                 if (Guess > numberToGuess)
                 {
-                    Console.WriteLine("You have guessed higher than the number. Keep guessing!\n\n");
+                    Console.WriteLine("\nYou have guessed higher than the number. Keep guessing!\n\n");
                     numGuesses++;
                 }
                 else if (Guess < numberToGuess)
                 {
-                    Console.WriteLine("You have guessed lower than the number. Keep guessing!\n\n");
+                    Console.WriteLine("\nYou have guessed lower than the number. Keep guessing!\n\n");
                     numGuesses++;
                 }
-                else
+                else if (Guess == numberToGuess)
                 {
                     numGuesses++;
+                    Console.Clear();
                     Console.WriteLine("Congratulations!!! You have guessed the correct number.  Such a genius, you are!\n");
                     Console.WriteLine("It took you " + numGuesses + " guesses to find the number.");
+                    hasWon = true;
                 }
-                break;
-                
-            }
 
+               
+            }
+            //game over
+            AddHighScore(numGuesses);
+            DisplayHighScores();
             
+        }
+
+
+        //add the highscores to the database
+        static void AddHighScore(int playerScore)
+        {
+            //get the name for the player
+            Console.WriteLine("\n\nYour name:\n");
+            string playerName = Console.ReadLine();
+            
+            //create a gateway to the database
+            DrewEntities db = new DrewEntities();
+ 
+            //create a new highscore object
+            HighScore newHighscore = new HighScore();
+            newHighscore.DateCreated = DateTime.Now;
+            newHighscore.Game = "Guess That Number";
+            newHighscore.Name = playerName;
+            newHighscore.Score = playerScore;
+
+            //add it to the database
+            db.HighScores.Add(newHighscore);
+
+
+            //always save changes, but only need to type this one time
+            db.SaveChanges();
+
+        }
+
+        //create a new function to display the highschore
+        static void DisplayHighScores()
+        {
+            //tell user to view highscores
+            Console.WriteLine("\n\nPress any key to view high scores...");
+            //clear console everytime to show highscore on a blank screen
+            Console.ReadKey();
+            Console.Clear();
+            //write the high score text
+            Console.WriteLine("Guess That Number High Scores!");
+            Console.WriteLine("==============================");
+            //create a new connection to the database
+            DrewEntities db = new DrewEntities();
+            //get the high score list
+            //pull from our HighScores database where the game is "Guess That Number, order by highest score, and only take the top 10 scores
+            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Guess That Number").OrderBy(x => x.Score).Take(10).ToList();
+
+            //make a foreach loop to print out these scores to the console
+            foreach (var highScore in highScoreList)
+            {
+                Console.WriteLine("{0}. {1} - {2} on {3}", highScoreList.IndexOf(highScore) + 1, highScore.Name, highScore.Score, highScore.DateCreated);
+            }
         }
     }
 }
