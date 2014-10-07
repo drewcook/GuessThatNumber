9e4f36c248a769cee64df32a94b8ca821120c2f3
SeedPaths-User <lessdismissive@email.com>
2014-10-07 09:12:44 -0600

    GuessThatNumber

--- GuessThatNumber/GuessThatNumber/Program.cs
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -65,60 +65,64 @@ namespace GuessThatNumber
                
             }
             //game over
-            AddHighScore(numGuesses);
-            DisplayHighScores();
+
+            Console.WriteLine("\n\n\nThank you for playing Guess That Number!!!");
+            Console.WriteLine("\n\n\nPress any key to exit...");
+            Console.ReadLine();
+            //AddHighScore(numGuesses);
+            //DisplayHighScores();
             
         }
 
 
-        //add the highscores to the database
-        static void AddHighScore(int playerScore)
-        {
-            //get the name for the player
-            Console.WriteLine("\n\nYour name:\n");
-            string playerName = Console.ReadLine();
+        ////add the highscores to the database
+        //static void AddHighScore(int playerScore)
+        //{
+        //    //get the name for the player
+        //    Console.WriteLine("\n\nYour name:\n");
+        //    string playerName = Console.ReadLine();
             
-            //create a gateway to the database
-            spDrewEntities db = new spDrewEntities();
+        //    //create a gateway to the database
+        //    spDrewEntities db = new spDrewEntities();
  
-            //create a new highscore object
-            HighScore newHighscore = new HighScore();
-            newHighscore.DateCreated = DateTime.Now;
-            newHighscore.Game = "Guess That Number";
-            newHighscore.Name = playerName;
-            newHighscore.Score = playerScore;
+        //    //create a new highscore object
+        //    HighScore newHighscore = new HighScore();
+        //    newHighscore.DateCreated = DateTime.Now;
+        //    newHighscore.Game = "Guess That Number";
+        //    newHighscore.Name = playerName;
+        //    newHighscore.Score = playerScore;
 
-            //add it to the database
-            db.HighScores.Add(newHighscore);
+        //    //add it to the database
+        //    db.HighScores.Add(newHighscore);
 
 
-            //always save changes, but only need to type this one time
-            db.SaveChanges();
+        //    //always save changes, but only need to type this one time
+        //    db.SaveChanges();
 
-        }
+        //}
 
-        //create a new function to display the highschore
-        static void DisplayHighScores()
-        {
-            //tell user to view highscores
-            Console.WriteLine("\n\nPress any key to view high scores...");
-            //clear console everytime to show highscore on a blank screen
-            Console.ReadKey();
-            Console.Clear();
-            //write the high score text
-            Console.WriteLine("Guess That Number High Scores!");
-            Console.WriteLine("==============================");
-            //create a new connection to the database
-            spDrewEntities db = new spDrewEntities();
-            //get the high score list
-            //pull from our HighScores database where the game is "Guess That Number, order by highest score, and only take the top 10 scores
-            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Guess That Number").OrderBy(x => x.Score).Take(10).ToList();
+        ////create a new function to display the highschore
+        //static void DisplayHighScores()
+        //{
+        //    //tell user to view highscores
+        //    Console.WriteLine("\n\nPress any key to view high scores...");
+        //    //clear console everytime to show highscore on a blank screen
+        //    Console.ReadKey();
+        //    Console.Clear();
+        //    //write the high score text
+        //    Console.WriteLine("Guess That Number High Scores!");
+        //    Console.WriteLine("==============================");
+        //    //create a new connection to the database
+        //    spDrewEntities db = new spDrewEntities();
+        //    //get the high score list
+        //    //pull from our HighScores database where the game is "Guess That Number, order by highest score, and only take the top 10 scores
+        //    List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Guess That Number").OrderBy(x => x.Score).Take(10).ToList();
 
-            //make a foreach loop to print out these scores to the console
-            foreach (var highScore in highScoreList)
-            {
-                Console.WriteLine("{0}. {1} - {2} on {3}", highScoreList.IndexOf(highScore) + 1, highScore.Name, highScore.Score, highScore.DateCreated);
-            }
-        }
+        //    //make a foreach loop to print out these scores to the console
+        //    foreach (var highScore in highScoreList)
+        //    {
+        //        Console.WriteLine("{0}. {1} - {2} on {3}", highScoreList.IndexOf(highScore) + 1, highScore.Name, highScore.Score, highScore.DateCreated);
+        //    }
+        //}
     }
 }
